ce31c42efbc584d4887fd575da8f113575ec78c0
Brook Hutchinson <Brookhutchinson@outlook.com>
2021-09-03 07:54:17 -0500

    Create new module named HomeModule

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,16 @@
+// angular
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    // angular modules
+    CommonModule
+  ],
+  declarations: [],
+  exports: [
+    // angular modules
+    CommonModule
+  ]
+})
+export class HomeModule {}
