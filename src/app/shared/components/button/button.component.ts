e68852052511a2b4a2a3e7661566a494581e3bfa
Brook Hutchinson <Brookhutchinson@outlook.com>
2021-10-03 08:18:59 -0500

    Emit btnClick event from ButtonComponent

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,6 +1,6 @@
 // angular
 import { Component, OnInit } from '@angular/core';
-import { Input } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -11,11 +11,13 @@ export class ButtonComponent implements OnInit {
   @Input() color: string;
   @Input() text: string;
 
+  @Output() btnClick = new EventEmitter;
+
   constructor() {}
 
   ngOnInit(): void {}
 
   onClick(): void {
-    console.log('Add Button onClick()');
+    this.btnClick.emit();
   }
 }
